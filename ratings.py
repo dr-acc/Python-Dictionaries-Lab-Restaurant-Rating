d4b88491bba1a8ce4a1376f0ff3240742e1b662e
Your Name <you@example.com>
2022-12-19 16:33:30 -0800

    Completed main portion of lab

--- ratings.py
+++ ratings.py
@@ -5,20 +5,29 @@ def add_rating(file):
     ratings_library = {}
 
     for line in opened_file:
-        restaurant_and_rating = line.split(":")
-        
+        restaurant_and_rating = line.rstrip().split(":")
+        ratings_library[restaurant_and_rating[0]] = int(restaurant_and_rating[1])
+
+        #now what do we need to do with each line?
+        #iterate over and pull out the names & ratings
+        #for word in restaurant_and_rating:
+            # [first section = restaurant name = key]
+            # [second section is just a number(the rating) = value]
     
-        print(restaurant = restaurant_and_rating[0])
-        print(rating = restaurant_and_rating[1])
+        # restaurant = restaurant_and_rating[0]
+        # rating = restaurant_and_rating[1]
         # ratings_library[rating] = #automatic 
     
-    # # unpack each line with .items()
-    # for restaurant, rating in ratings_library.items():
-    #     print(restaurant, rating)
+    # # unpack and sort each line with .items() and sorted
+    for restaurant, rating in sorted(ratings_library.items()):
+        print(f'{restaurant} is rated at {rating}.')
 
 add_rating("scores.txt")
 
 # put your code here
 # adding (brainstorm) animals["pony"]=2
 #ratings_library["rating"]=value
-ratings_library.items()
+# ratings_library.items()
+
+
+# words_dict[word] = words_dict.get(word, 0) + 1
